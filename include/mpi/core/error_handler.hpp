15637a248505d0fb24a20c188c96a1b75aaede67
acdemiralp <demiralpali@gmail.com>
2021-10-01 20:20:07 +0200

    Started implementing error handler.

---
+++ include/mpi/core/error_handler.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+
+#include <mpi/communicators/communicator.hpp>
+#include <mpi/core/mpi.hpp>
+#include <mpi/core/return_code.hpp>
+
+namespace mpi
+{
+class error_handler
+{
+public:
+  error_handler           (std::function<void(communicator&, return_code)> function)
+  : function_(function)
+  {
+    MPI_Comm_create_errhandler([ ] (MPI_Comm* raw_communicator, std::int32_t* raw_return_code, ...)
+    {
+      function_(communicator(*raw_communicator), return_code(*raw_communicator));
+    }, &native_);
+  }
+  error_handler           (const error_handler&  that) = delete;
+  error_handler           (      error_handler&& temp) noexcept
+  : managed_(temp.managed_), native_(temp.native_)
+  {
+    temp.managed_ = false;
+    temp.native_  = MPI_ERRHANDLER_NULL;
+  }
+  virtual ~error_handler  ()
+  {
+    if (managed_ && native_ != MPI_ERRHANDLER_NULL)
+      MPI_Errhandler_free(&native_);
+  }
+  error_handler& operator=(const error_handler&  that) = delete;
+  error_handler& operator=(      error_handler&& temp) noexcept
+  {
+    if (this != &temp)
+    {
+      managed_      = temp.managed_;
+      native_       = temp.native_ ;
+
+      temp.managed_ = false;
+      temp.native_  = MPI_ERRHANDLER_NULL;
+    }
+    return *this;
+  }
+
+protected:
+  bool                                            managed_ = false;
+  MPI_Errhandler                                  native_  = MPI_ERRHANDLER_NULL;
+  std::function<void(communicator&, return_code)> function_;
+};
+}
