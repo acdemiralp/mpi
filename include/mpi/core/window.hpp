5eab3f447b7bd9f559b01951f9ec008f8ece3c41
acdemiralp <demiralpali@gmail.com>
2021-10-09 16:02:11 +0200

    Removed extra window_key_value.

--- include/mpi/core/window.hpp
+++ include/mpi/core/window.hpp
@@ -13,65 +13,6 @@
 
 namespace mpi
 {
-class window_key_value
-{
-public:
-  using copy_function   = std::int32_t (*) (MPI_Win, std::int32_t, void*, void*, void*, std::int32_t*);
-  using delete_function = std::int32_t (*) (MPI_Win, std::int32_t, void*, void*);
-
-  explicit window_key_value  (
-    const copy_function   copy_function   = MPI_WIN_DUP_FN        , 
-    const delete_function delete_function = MPI_WIN_NULL_DELETE_FN, 
-    void*                 extra_state     = nullptr)
-  : managed_(true)
-  {
-    MPI_CHECK_ERROR_CODE(MPI_Win_create_keyval, (copy_function, delete_function, &native_, extra_state)) // Extra state is unused.
-  }
-  window_key_value           (const window_key_value&  that) = delete;
-  window_key_value           (      window_key_value&& temp) noexcept
-  : managed_(temp.managed_), native_(temp.native_)
-  {
-    temp.managed_ = false;
-    temp.native_  = MPI_KEYVAL_INVALID;
-  }
-  virtual ~window_key_value  ()
-  {
-    if (managed_ && native_ != MPI_KEYVAL_INVALID)
-      MPI_CHECK_ERROR_CODE(MPI_Win_free_keyval, (&native_))
-  }
-  window_key_value& operator=(const window_key_value&  that) = delete;
-  window_key_value& operator=(      window_key_value&& temp) noexcept
-  {
-    if (this != &temp)
-    {
-      if (managed_ && native_ != MPI_KEYVAL_INVALID)
-        MPI_CHECK_ERROR_CODE(MPI_Win_free_keyval, (&native_))
-
-      managed_      = temp.managed_;
-      native_       = temp.native_;
-
-      temp.managed_ = false;
-      temp.native_  = MPI_KEYVAL_INVALID;
-    }
-    return *this;
-  }
-
-  [[nodiscard]]
-  bool         managed() const
-  {
-    return managed_;
-  }
-  [[nodiscard]]
-  std::int32_t native () const
-  {
-    return native_;
-  }
-
-protected:
-  bool         managed_ = false;
-  std::int32_t native_  = MPI_KEYVAL_INVALID;
-};
-
 class window
 {
 public:
