1ef5913a6ff41b9cfe5d6da60adfc53e235d42d6
acdemiralp <demiralpali@gmail.com>
2021-10-09 17:05:10 +0200

    Key value also got a base class for consistency.

--- include/mpi/core/key_value.hpp
+++ include/mpi/core/key_value.hpp
@@ -7,7 +7,44 @@
 
 namespace mpi
 {
-class communicator_key_value
+class key_value
+{
+public:
+  explicit key_value  (const std::int32_t native)
+  : native_(native)
+  {
+    
+  }
+  key_value           (const key_value&  that) = delete ;
+  key_value           (      key_value&& temp) noexcept
+  : managed_(temp.managed_), native_(temp.native_)
+  {
+    temp.managed_ = false;
+    temp.native_  = MPI_KEYVAL_INVALID;
+  }
+  virtual ~key_value  ()                       = default;
+  key_value& operator=(const key_value&  that) = delete ;
+  key_value& operator=(      key_value&& temp) = default;
+
+  [[nodiscard]]
+  bool         managed() const
+  {
+    return managed_;
+  }
+  [[nodiscard]]
+  std::int32_t native () const
+  {
+    return native_;
+  }
+
+protected:
+  key_value() = default; // Default constructor is only available to sub classes who control the member variables explicitly.
+
+  bool         managed_ = false;
+  std::int32_t native_  = MPI_KEYVAL_INVALID;
+};
+
+class communicator_key_value : public key_value
 {
 public:
   using copy_function_type   = std::int32_t (*) (MPI_Comm, std::int32_t, void*, void*, void*, std::int32_t*);
@@ -17,23 +54,23 @@ public:
     const copy_function_type   copy_function   = MPI_COMM_DUP_FN        ,
     const delete_function_type delete_function = MPI_COMM_NULL_DELETE_FN,
     void*                      extra_state     = nullptr                )
-  : managed_(true)
   {
+    managed_ = true;
     MPI_CHECK_ERROR_CODE(MPI_Comm_create_keyval, (copy_function, delete_function, &native_, extra_state))
   }
-  communicator_key_value           (const communicator_key_value&  that) = delete;
-  communicator_key_value           (      communicator_key_value&& temp) noexcept
-  : managed_(temp.managed_), native_(temp.native_)
+  explicit communicator_key_value  (const std::int32_t native)
+  : key_value(native)
   {
-    temp.managed_ = false;
-    temp.native_  = MPI_KEYVAL_INVALID;
+    
   }
-  virtual ~communicator_key_value  ()
+  communicator_key_value           (const communicator_key_value&  that)          = delete ;
+  communicator_key_value           (      communicator_key_value&& temp) noexcept = default;
+ ~communicator_key_value           ()                                    override
   {
     if (managed_ && native_ != MPI_KEYVAL_INVALID)
       MPI_CHECK_ERROR_CODE(MPI_Comm_free_keyval, (&native_))
   }
-  communicator_key_value& operator=(const communicator_key_value&  that) = delete;
+  communicator_key_value& operator=(const communicator_key_value&  that)          = delete ;
   communicator_key_value& operator=(      communicator_key_value&& temp) noexcept
   {
     if (this != &temp)
@@ -49,24 +86,9 @@ public:
     }
     return *this;
   }
-
-  [[nodiscard]]
-  bool         managed() const
-  {
-    return managed_;
-  }
-  [[nodiscard]]
-  std::int32_t native () const
-  {
-    return native_;
-  }
-
-protected:
-  bool         managed_ = false;
-  std::int32_t native_  = MPI_KEYVAL_INVALID;
 };
-
-class data_type_key_value
+  
+class data_type_key_value : public key_value
 {
 public:
   using copy_function_type   = std::int32_t (*) (MPI_Datatype, std::int32_t, void*, void*, void*, std::int32_t*);
@@ -76,23 +98,23 @@ public:
     const copy_function_type   copy_function   = MPI_TYPE_DUP_FN        ,
     const delete_function_type delete_function = MPI_TYPE_NULL_DELETE_FN,
     void*                      extra_state     = nullptr                )
-  : managed_(true)
   {
+    managed_ = true;
     MPI_CHECK_ERROR_CODE(MPI_Type_create_keyval, (copy_function, delete_function, &native_, extra_state))
   }
-  data_type_key_value           (const data_type_key_value&  that) = delete;
-  data_type_key_value           (      data_type_key_value&& temp) noexcept
-  : managed_(temp.managed_), native_(temp.native_)
+  explicit data_type_key_value  (const std::int32_t native)
+  : key_value(native)
   {
-    temp.managed_ = false;
-    temp.native_  = MPI_KEYVAL_INVALID;
+    
   }
-  virtual ~data_type_key_value  ()
+  data_type_key_value           (const data_type_key_value&  that)          = delete ;
+  data_type_key_value           (      data_type_key_value&& temp) noexcept = default;
+ ~data_type_key_value           ()                                 override
   {
     if (managed_ && native_ != MPI_KEYVAL_INVALID)
       MPI_CHECK_ERROR_CODE(MPI_Type_free_keyval, (&native_))
   }
-  data_type_key_value& operator=(const data_type_key_value&  that) = delete;
+  data_type_key_value& operator=(const data_type_key_value&  that)          = delete ;
   data_type_key_value& operator=(      data_type_key_value&& temp) noexcept
   {
     if (this != &temp)
@@ -108,24 +130,9 @@ public:
     }
     return *this;
   }
-
-  [[nodiscard]]
-  bool         managed() const
-  {
-    return managed_;
-  }
-  [[nodiscard]]
-  std::int32_t native () const
-  {
-    return native_;
-  }
-
-protected:
-  bool         managed_ = false;
-  std::int32_t native_  = MPI_KEYVAL_INVALID;
 };
-
-class window_key_value
+  
+class window_key_value : public key_value
 {
 public:
   using copy_function_type   = std::int32_t (*) (MPI_Win, std::int32_t, void*, void*, void*, std::int32_t*);
@@ -135,23 +142,23 @@ public:
     const copy_function_type   copy_function   = MPI_WIN_DUP_FN        ,
     const delete_function_type delete_function = MPI_WIN_NULL_DELETE_FN,
     void*                      extra_state     = nullptr               )
-  : managed_(true)
   {
+    managed_ = true;
     MPI_CHECK_ERROR_CODE(MPI_Win_create_keyval, (copy_function, delete_function, &native_, extra_state))
   }
-  window_key_value           (const window_key_value&  that) = delete;
-  window_key_value           (      window_key_value&& temp) noexcept
-  : managed_(temp.managed_), native_(temp.native_)
+  explicit window_key_value  (const std::int32_t native)
+  : key_value(native)
   {
-    temp.managed_ = false;
-    temp.native_  = MPI_KEYVAL_INVALID;
+    
   }
-  virtual ~window_key_value  ()
+  window_key_value           (const window_key_value&  that)          = delete ;
+  window_key_value           (      window_key_value&& temp) noexcept = default;
+ ~window_key_value           ()                              override
   {
     if (managed_ && native_ != MPI_KEYVAL_INVALID)
       MPI_CHECK_ERROR_CODE(MPI_Win_free_keyval, (&native_))
   }
-  window_key_value& operator=(const window_key_value&  that) = delete;
+  window_key_value& operator=(const window_key_value&  that)          = delete ;
   window_key_value& operator=(      window_key_value&& temp) noexcept
   {
     if (this != &temp)
@@ -167,20 +174,5 @@ public:
     }
     return *this;
   }
-
-  [[nodiscard]]
-  bool         managed() const
-  {
-    return managed_;
-  }
-  [[nodiscard]]
-  std::int32_t native () const
-  {
-    return native_;
-  }
-
-protected:
-  bool         managed_ = false;
-  std::int32_t native_  = MPI_KEYVAL_INVALID;
 };
 }
