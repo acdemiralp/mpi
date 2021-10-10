8b5c9c6508840d6426a0c334e401f39ed8353d78
acdemiralp <demiralpali@gmail.com>
2021-10-11 00:29:22 +0200

    Added std::function constructors to generalized_request by using the extra state pointer.

--- include/mpi/core/generalized_request.hpp
+++ include/mpi/core/generalized_request.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <cstdint>
+#include <functional>
 
+#include <mpi/core/error/error_code.hpp>
 #include <mpi/core/exception.hpp>
 #include <mpi/core/mpi.hpp>
 #include <mpi/core/request.hpp>
@@ -24,6 +26,27 @@ public:
     managed_ = true;
     MPI_CHECK_ERROR_CODE(MPI_Grequest_start, (query_function, free_function, cancel_function, extra_state, &native_))
   }
+  explicit generalized_request  (
+    const std::function<error_code(MPI_Status&)>& query_function , 
+    const std::function<error_code()>&            free_function  ,
+    const std::function<error_code(bool)>&        cancel_function)
+  : query_function_(query_function), free_function_(free_function), cancel_function_(cancel_function)
+  {
+    managed_ = true;
+    MPI_CHECK_ERROR_CODE(MPI_Grequest_start, (
+      [ ] (void* this_pointer, MPI_Status* status)
+      {
+        return static_cast<generalized_request*>(this_pointer)->query_function_ (*status)                    .error_class().native();
+      }, 
+      [ ] (void* this_pointer)
+      {
+        return static_cast<generalized_request*>(this_pointer)->free_function_  ()                           .error_class().native();
+      },
+      [ ] (void* this_pointer, const std::int32_t complete)
+      {
+        return static_cast<generalized_request*>(this_pointer)->cancel_function_(static_cast<bool>(complete)).error_class().native();
+      }, this, &native_))
+  }
   explicit generalized_request  (const MPI_Request native)
   : request(native)
   {
@@ -39,5 +62,10 @@ public:
   {
     MPI_CHECK_ERROR_CODE(MPI_Grequest_complete, (native_))
   }
+
+protected:
+  std::function<error_code(MPI_Status&)> query_function_ ;
+  std::function<error_code()>            free_function_  ;
+  std::function<error_code(bool)>        cancel_function_;
 };
 }
