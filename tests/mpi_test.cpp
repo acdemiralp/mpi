bea0f0da6616447c5ba16be55b6fd5e0d29a0d69
acdemiralp <demiralpali@gmail.com>
2021-10-26 06:42:03 +0200

    Added a simple send/recv test.

--- tests/mpi_test.cpp
+++ tests/mpi_test.cpp
@@ -1,8 +1,24 @@
 #include "internal/doctest.h"
 
+#include <iostream>
+
 #include <mpi/all.hpp>
 
 TEST_CASE("MPI Test")
 {
-  mpi::environment environment;
+  mpi::environment         environment  ;
+  const mpi::communicator& communicator = mpi::world_communicator;
+
+  std::int32_t data = 0;
+  if (communicator.rank() == 0)
+  {
+    data = 42;
+    communicator.send   (data, 1);
+  }
+  if (communicator.rank() == 1)
+  {
+    communicator.receive(data, 0);
+    REQUIRE(data == 42);
+    std::cout << data << std::endl;
+  }
 }
