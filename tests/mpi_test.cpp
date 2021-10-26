a8e20d5a938ee2702f7e7ee25578113970aaad4d
acdemiralp <demiralpali@gmail.com>
2021-10-26 07:16:32 +0200

    Added a container of compliant test.

--- tests/mpi_test.cpp
+++ tests/mpi_test.cpp
@@ -9,6 +9,7 @@ TEST_CASE("MPI Test")
   mpi::environment         environment  ;
   const mpi::communicator& communicator = mpi::world_communicator;
 
+  // Single compliant test.
   std::int32_t data = 0;
   if (communicator.rank() == 0)
   {
@@ -20,5 +21,21 @@ TEST_CASE("MPI Test")
     communicator.receive(data, 0);
     REQUIRE(data == 42);
     std::cout << data << std::endl;
+  }
+
+  // Container of compliant test.
+  std::vector<std::int32_t> data_container(3);
+  if (communicator.rank() == 0)
+  {
+    data_container = {1, 2, 3};
+    communicator.send   (data_container, 1);
+  }
+  if (communicator.rank() == 1)
+  {
+    communicator.receive(data_container, 0);
+    REQUIRE(data_container[0] == 1);
+    REQUIRE(data_container[1] == 2);
+    REQUIRE(data_container[2] == 3);
+    std::cout << data_container[0] << " " << data_container[1] << " " << data_container[2] << std::endl;
   }
 }
